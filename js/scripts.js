a7c95938240cc7ce63a1e3e2a4d3997f89db7669
Patrick Wadden <patrickwadden@Patricks-MBP.gateway>
2017-05-15 16:50:34 +1000

    layout one desktop

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,87 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-        //Navigation
-        // menu: '#menu',
-        // lockAnchors: false,
-        // anchors:['firstPage', 'secondPage'],
-        // navigation: false,
-        // navigationPosition: 'right',
-        // navigationTooltips: ['firstSlide', 'secondSlide'],
-        // showActiveTooltip: false,
-        // slidesNavigation: false,
-        // slidesNavPosition: 'bottom',
+  $('#fullpage').fullpage({
+      //Navigation
+      // menu: '#menu',
+      // lockAnchors: false,
+      // anchors:['firstPage', 'secondPage'],
+      // navigation: false,
+      // navigationPosition: 'right',
+      // navigationTooltips: ['firstSlide', 'secondSlide'],
+      // showActiveTooltip: false,
+      // slidesNavigation: false,
+      // slidesNavPosition: 'bottom',
 
-        //Scrolling
-        // css3: true,
-        // scrollingSpeed: 700,
-        // autoScrolling: true,
-        // fitToSection: true,
-        // fitToSectionDelay: 1000,
-        // scrollBar: false,
-        // easing: 'easeInOutCubic',
-        // easingcss3: 'ease',
-        // loopBottom: false,
-        // loopTop: false,
-        // loopHorizontal: true,
-        // continuousVertical: false,
-        // continuousHorizontal: false,
-        // scrollHorizontally: false,
-        // interlockedSlides: false,
-        // dragAndMove: false,
-        // offsetSections: false,
-        // resetSliders: false,
-        // fadingEffect: false,
-        // normalScrollElements: '#element1, .element2',
-        // scrollOverflow: false,
-        // scrollOverflowReset: false,
-        // scrollOverflowOptions: null,
-        // touchSensitivity: 15,
-        // normalScrollElementTouchThreshold: 5,
-        // bigSectionsDestination: null,
+      //Scrolling
+      // css3: true,
+      // scrollingSpeed: 700,
+      // autoScrolling: true,
+      // fitToSection: true,
+      // fitToSectionDelay: 1000,
+      // scrollBar: false,
+      // easing: 'easeInOutCubic',
+      // easingcss3: 'ease',
+      // loopBottom: false,
+      // loopTop: false,
+      // loopHorizontal: true,
+      // continuousVertical: false,
+      // continuousHorizontal: false,
+      // scrollHorizontally: false,
+      // interlockedSlides: false,
+      // dragAndMove: false,
+      // offsetSections: false,
+      // resetSliders: false,
+      // fadingEffect: false,
+      // normalScrollElements: '#element1, .element2',
+      // scrollOverflow: false,
+      // scrollOverflowReset: false,
+      // scrollOverflowOptions: null,
+      // touchSensitivity: 15,
+      // normalScrollElementTouchThreshold: 5,
+      // bigSectionsDestination: null,
 
-        //Accessibility
-        // keyboardScrolling: true,
-        // animateAnchor: true,
-        // recordHistory: true,
+      //Accessibility
+      // keyboardScrolling: true,
+      // animateAnchor: true,
+      // recordHistory: true,
 
-        //Design
-        // controlArrows: true,
-        verticalCentered: false,
-        // sectionsColor : ['#ccc', '#fff'],
-        // paddingTop: '3em',
-        // paddingBottom: '10px',
-        // fixedElements: '#header, .footer',
-        // responsiveWidth: 0,
-        // responsiveHeight: 0,
-        // responsiveSlides: false,
-        // parallax: false,
-        // parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+      //Design
+      // controlArrows: true,
+      verticalCentered: false,
+      // sectionsColor : ['#ccc', '#fff'],
+      // paddingTop: '3em',
+      // paddingBottom: '10px',
+      // fixedElements: '#header, .footer',
+      // responsiveWidth: 0,
+      // responsiveHeight: 0,
+      // responsiveSlides: false,
+      // parallax: false,
+      // parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-        //Custom selectors
-        // sectionSelector: '.section',
-        // slideSelector: '.slide',
+      //Custom selectors
+      // sectionSelector: '.section',
+      // slideSelector: '.slide',
 
-        // lazyLoading: true,
+      // lazyLoading: true,
 
-        //events
-        // onLeave: function(index, nextIndex, direction){},
-        // afterLoad: function(anchorLink, index){},
-        // afterRender: function(){},
-        // afterResize: function(){},
-        // afterResponsive: function(isResponsive){},
-        // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        // onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-    });
-    matchHeight($('div[class^="image-"]'));
+      //events
+      // onLeave: function(index, nextIndex, direction){},
+      // afterLoad: function(anchorLink, index){},
+      // afterRender: function(){},
+      // afterResize: function(){},
+      // afterResponsive: function(isResponsive){},
+      // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+      // onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+  });
+
+  $( ".layout-one" ).each(function(index, element) {
+    layoutOneImageOneHeight(element);
+  });
 });
 
-// function matchHeight(el) {
-//     width = el.outerWidth();
-//     el.outerHeight(width);
-// }
+function layoutOneImageOneHeight(section) {
+  topHeight = $(section).find('.top .left').height();
+  introHeight = $(section).find('.top .left .intro').outerHeight();
+  padding = parseInt($(section).find('.top .left .intro').css('padding'));
+  imageOneHeight = topHeight - introHeight - (padding * 2);
+  $(section).find('.top .image-one').height(imageOneHeight);
+}
